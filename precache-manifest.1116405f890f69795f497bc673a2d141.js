8151cb5ca0ec735e7f23b2589b4b64f8eed0497e
Tracie T <traciecodes@gmail.com>
2019-12-31 20:26:10 -0600

    Updates

---
+++ precache-manifest.1116405f890f69795f497bc673a2d141.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e141a6887bae2445b2c3ba8085d6d9b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1921fa1341f7ee612b48",
+    "url": "/monsters-rolodex/static/css/main.a17fbbec.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1921fa1341f7ee612b48",
+    "url": "/monsters-rolodex/static/js/main.d78bee15.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
